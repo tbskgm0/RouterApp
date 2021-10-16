1e6fdebfcf00eb6bb50a77ec118cb8738b6edfe9
TsubasaKogoma <tsubasa.kogoma@axia-work.com>
2021-10-16 13:13:42 +0900

    Initial Commit

---
+++ RouterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RouterApp
+//
+//  Created by TsubasaKogoma on 2021/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
